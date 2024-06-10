9e6dbf74582dc837e27ba8725d29acbbab7cc3b3
henrique <henrsamp0@gmail.com>
2024-06-10 16:00:30 -0300

    teste

---
+++ app/Views/home/index.php
@@ -0,0 +1,23 @@
+<div class="container p-4">
+    <img src="<?=base_url("assets/img/logoeep.jpg")?>"  class="float-end me-3 img-thumbnail"> 
+    <h2>Bem-vindos à Biblioteca da EEEP Walter Ramos de Araujo!</h2>
+    <br>
+    <div class="row p-4">
+        <div class="col-7">
+            <p>É com grande alegria que abrimos as portas deste espaço dedicado ao conhecimento, à imaginação e ao aprendizado. Aqui, 
+            vocês encontrarão não apenas livros, 
+            mas um universo de possibilidades para desafiar ideias e explorar novos mundos.
+            </p>
+            <p>Nossa biblioteca é mais do que um simples local de leitura; é um refúgio para mentes curiosas, 
+                um ponto de encontro para aqueles que buscam descobrir e compartilhar saberes. 
+            </p>
+            <p>
+            Convidamos todos vocês a explorarem, questionarem e se maravilharem com o vasto acervo que temos à disposição.
+            </p>
+            <p>
+            Sejam bem-vindos à Biblioteca da EEEP Walter Ramos de Araujo, 
+            onde os sonhos ganham asas e o conhecimento se torna uma jornada sem fim.
+            </p>
+        </div>
+    </div>
+</div>   
